60a609eb2898ed3e915faac56a8c1efbe5023cb0
emluo555 <emilyluo555@gmail.com>
2025-11-24 02:35:12 -0500

    tam visualization wip

---
+++ vis_tam/run_tam.py
@@ -0,0 +1,114 @@
+import os
+import torch
+from transformers import Qwen3VLForConditionalGeneration, AutoProcessor
+from qwen_utils import process_vision_info
+from PIL import Image
+from tam import TAM
+import argparse
+
+
+def tam_demo_for_qwen3_vl(image_path, prompt_text, save_dir='vis_results'):
+    # Load Qwen2-VL model and processor
+    # huge note to self: Qwen2-VL-7B does not work
+    model_name = "Qwen/Qwen3-VL-8B-Instruct"
+    
+    model = Qwen3VLForConditionalGeneration.from_pretrained(
+        model_name, torch_dtype="auto", device_map="auto", local_files_only=True
+    )
+    processor = AutoProcessor.from_pretrained(model_name, local_files_only=True)
+    # img = Image.open(image_path).convert("RGB")
+
+    # Prepare input message with image/video and prompt
+    if isinstance(image_path, list):
+        # this might not work if video param doesn't support image_path
+        messages = [{"role": "user", "content": [{"type": "video", "video": image_path}, {"type": "text", "text": prompt_text}]}]
+    else:
+        messages = [{"role": "user", "content": [{"type": "image", "image": image_path}, {"type": "text", "text": prompt_text}]}]
+
+    # Process input text and visual info
+    # print("messages ", messages)
+    text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    # print("TEXT ", type(text))
+    image_inputs, video_inputs = process_vision_info(messages)
+    # print("image_inputs and video_inputs: ", image_inputs, video_inputs)
+    inputs = processor(text=[text], images=image_inputs, videos=video_inputs, padding=True, return_tensors="pt")
+    inputs = inputs.to(model.device)
+    # print("inputs ", inputs)
+
+    # Generate model output with hidden states for visualization
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=256,
+        use_cache=True,
+        output_hidden_states=True, # ---> TAM needs hidden states
+        return_dict_in_generate=True
+    )
+
+    generated_ids = outputs.sequences
+
+    # === TAM code part ====
+
+    # Compute logits from last hidden states with vocab classifier for TAM
+    logits = [model.lm_head(feats[-1]) for feats in outputs.hidden_states]
+
+    # Define special token IDs to separate image/prompt/answer tokens
+    # See TAM in tam.py about its usage. See ids from the specific model.
+    special_ids = {
+        'img_id': [151652, 151653],  
+        'prompt_id': [151653, [151645, 198, 151644, 77091]], 
+        'answer_id': [[198, 151644, 77091, 198], -1] 
+    }
+
+    # get shape of vision output
+    if isinstance(image_path, list):
+        vision_shape = (inputs['video_grid_thw'][0, 0], inputs['video_grid_thw'][0, 1] // 2, inputs['video_grid_thw'][0, 2] // 2)
+    else:
+        vision_shape = (inputs['image_grid_thw'][0, 1] // 2, inputs['image_grid_thw'][0, 2] // 2)
+
+    # get img or video inputs for next vis
+    vis_inputs = [[video_inputs[0][i] for i in range(0, len(video_inputs[0]))]] if isinstance(image_path, list) else image_inputs
+
+    # === TAM Visualization ===
+    # Call TAM() to generate token activation map for each generation round
+    # Arguments:
+    # - token ids (inputs and generations)
+    # - shape of vision token
+    # - logits for each round
+    # - special token identifiers for localization
+    # - image / video inputs for visualization
+    # - processor for decoding
+    # - output image path to save the visualization
+    # - round index (0 here)
+    # - raw_vis_records: list to collect intermediate visualization data
+    # - eval only, False to vis
+    # return TAM vision map for eval, saving multimodal TAM in the function
+
+    raw_map_records = []
+    for i in range(len(logits)):
+        img_map = TAM(
+            generated_ids[0].cpu().tolist(),
+            vision_shape,
+            logits,
+            special_ids,
+            vis_inputs,
+            processor,
+            os.path.join(save_dir, str(i) + '.jpg'),
+            i,
+            raw_map_records,
+            False)
+
+
+if __name__ == "__main__":
+    # single img demo (qwen)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_path", type=str, required=True)
+    parser.add_argument("--prompt", type=str, required=True)
+    parser.add_argument("--img_save_dir", type=str, required=True)
+    args = parser.parse_args()
+
+    img_path = args.img_path
+    prompt = args.prompt
+    img_save_dir = args.img_save_dir
+   
+    tam_demo_for_qwen3_vl(img_path, prompt, save_dir=img_save_dir)
+
