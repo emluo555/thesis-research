a6e8a3bd8a84711da536769335d2112d215d3c5e
emluo555 <emilyluo555@gmail.com>
2025-12-09 02:54:18 -0500

    run tam vis on vis node

---
+++ scripts/run_tam.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILE_NAME="graph" # circle_radius
+PROMPT="What is the difference between two consecutive major ticks on the Y-axis? Please respond with one integer"
+
+cd vis_tam
+
+# NON-THINKING
+python run_tam.py \
+    --img_path ../sample_data/$FILE_NAME.jpg \
+    --prompt "$PROMPT" \
+    --img_save_dir ../sample_data/vis_thinking_2_$FILE_NAME \
+    --model_path "/scratch/gpfs/ZHUANGL/el5267/cache/huggingface/models--Qwen--Qwen3-VL-8B-Instruct/snapshots/0c351dd01ed87e9c1b53cbc748cba10e6187ff3b" \
+    --model_name "Qwen3-VL-8B-Instruct"
+
+# # THINKING
+# python run_tam.py \
+#     --img_path ../sample_data/$FILE_NAME.jpg \
+#     --prompt "$PROMPT" \
+#     --img_save_dir ../sample_data/vis_thinking_cot_2_$FILE_NAME \
+#     --model_path "/scratch/gpfs/ZHUANGL/el5267/cache/huggingface/models--Qwen--Qwen3-VL-8B-Thinking/snapshots/41ea130ce6eaaf7829c72dfc0e4597d49741ed18" \
+#     --model_name "Qwen3-VL-8B-Thinking"
